4dac9f99c535b93c1113c69ab910100e0533eece
Garrett <me@garrettfoy.com>
2024-07-15 21:21:24 -0500

    add basic main to test salting and services

---
+++ cmd/api/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"garrettpfoy/orbit-api/internal/repositories/access_token"
+	// "garrettpfoy/orbit-api/internal/repositories/queue"
+	// "garrettpfoy/orbit-api/internal/repositories/session"
+	// "garrettpfoy/orbit-api/internal/repositories/user"
+	// "garrettpfoy/orbit-api/internal/repositories/vote"
+
+	"garrettpfoy/orbit-api/internal/services/encryption"
+
+	"garrettpfoy/orbit-api/internal/environment"
+
+	"garrettpfoy/orbit-api/internal/models"
+
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database: ", err)
+	}
+
+	environment, err := environment.LoadOrbitEnvironment(false)
+	if err != nil {
+		log.Fatal("failed to establish the orbit environment: ", err)
+	}
+
+	encryptionService := encryption.NewEncryptionService(environment.ENCRYPTION_SECRET)
+
+	// Set the encryption service in the AccessToken model
+	models.SetEncryptionService(encryptionService)
+
+	// Auto migrate the schema
+	db.AutoMigrate(&models.Session{}, &models.AccessToken{}, &models.Queue{}, &models.User{}, &models.Vote{})
+
+	accessTokenRepo := &access_token.GormAccessTokenRepository{DB: db}
+
+	// Example: Creating a new access token
+	newToken := models.AccessToken{
+		UserID:       1, // assuming user ID 1 exists
+		AccessToken:  "example_access_token",
+		RefreshToken: "example_refresh_token",
+		ExpiryTime:   time.Now().Add(time.Hour * 1),
+	}
+	err = accessTokenRepo.CreateAccessToken(&newToken)
+	if err != nil {
+		log.Fatalf("Failed to create access token: %v", err)
+	}
+
+	// Fetch and decrypt the access token
+	token, err := accessTokenRepo.GetAccessToken(newToken.ID)
+	if err != nil {
+		log.Fatalf("Failed to get access token: %v", err)
+	}
+	log.Printf("Decrypted Access Token: %s", token.AccessToken)
+	log.Printf("Decrypted Refresh Token: %s", token.RefreshToken)
+}
